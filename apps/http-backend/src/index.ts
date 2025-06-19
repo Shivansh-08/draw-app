e4ae2dd53d1375a901d320dcd2d01873f443da99
shivansh <shivanshsinghania827@gmail.com>
2025-06-19 19:17:18 +0530

    created basic routes for http backend and ws bckend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,31 @@
 import express  from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
 app.listen(3001);
+
+app.post('/signup',(req,res)=>{
+   const body = req.body;
+
+})
+
+app.post('/signin',(req,res)=>{
+
+
+  const token=  jwt.sign({
+
+    },JWT_SECRET)
+
+    res.json({
+        token
+    })
+
+})
+
+app.post('/room',middleware,(req,res)=>{
+
+
+})
